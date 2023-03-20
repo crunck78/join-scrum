1783eb1f7a8ff760d81bf7f63e8c5ba307ee7d97
mihaiandreineacsu <mihai@developerakademie.com>
2023-03-20 08:19:26 +0100

    style main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
   standalone: true,
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, HeaderComponent]
 })
 export class MainComponent {
 
