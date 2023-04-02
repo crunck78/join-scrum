0186a7a513d8083f3a2bc94b6c429dd9e7b58033
mihaiandreineacsu <mihai@developerakademie.com>
2023-04-02 21:33:49 +0200

    header layout and cta

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,28 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ScrumApiService } from '../scrum-api/scrum-api.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule
+  ]
 })
 export class HeaderComponent {
 
-  constructor(private scrumApi: ScrumApiService){
+  constructor(private scrumApi: ScrumApiService) {
 
   }
 
   @Output() toggleDrawer = new EventEmitter();
 
-  logout(){
-    this.scrumApi.apiToken$.next({token: ""});
+  logout() {
+    this.scrumApi.apiToken$.next({ token: "" });
   }
 
 }
