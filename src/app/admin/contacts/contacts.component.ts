3d30a69c109317a9ab360b79e7f0aabdd920ec5b
mihaiandreineacsu <mihai@developerakademie.com>
2023-04-04 08:38:14 +0200

    contacts layout

--- src/app/admin/contacts/contacts.component.ts
+++ src/app/admin/contacts/contacts.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { PageComponent } from '../page/page.component';
+import { CardComponent } from 'src/app/shared/shared-components/card/card.component';
+import { PageTitleComponent } from 'src/app/shared/shared-components/page-title/page-title.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule, CardComponent, PageTitleComponent, MatButtonModule
+  ]
 })
 export class ContactsComponent {
+
+  contacts = [];
+
+  openDialog(){
+
+  }
 
 }
--- src/app/admin/summary/summary.component.ts
+++ src/app/admin/summary/summary.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CardComponent } from 'src/app/shared/shared-components/card/card.component';
+import { PageTitleComponent } from 'src/app/shared/shared-components/page-title/page-title.component';
 
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss'],
+  standalone: true,
+  imports: [CardComponent,PageTitleComponent]
 })
 export class SummaryComponent {}
