7c7af2861e153f5a68a881c49bdea00d76ca3c23
mihaiandreineacsu <mihai@developerakademie.com>
2023-05-15 08:57:43 +0200

    add board drag drop layout

--- src/app/admin/board/board.component.ts
+++ src/app/admin/board/board.component.ts
@@ -3,18 +3,44 @@ import { PageComponent } from '../page/page.component';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from 'src/app/shared/shared-components/card/card.component';
 import { PageTitleComponent } from 'src/app/shared/shared-components/page-title/page-title.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
   standalone: true,
-  imports:[
+  imports: [
     CommonModule,
     CardComponent,
     PageTitleComponent,
+    DragDropModule,
+    MatButtonModule
   ]
 })
 export class BoardComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep', 'Get to work', 'Pick up groceries', 'Go home', 'Fall asleep', 'Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  backlog = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  addList(){
+
+  }
 }
