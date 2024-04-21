4ce765385bfe8bf820dcd9e0b7b6110fd08b6df8
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-04-21 11:30:20 +0200

    bind legal-notice iframe from my personal page

--- src/app/admin/legal-notice/legal-notice.component.ts
+++ src/app/admin/legal-notice/legal-notice.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from 'src/app/shared/shared-components/card/card.component';
 
 @Component({
   selector: 'app-legal-notice',
   templateUrl: './legal-notice.component.html',
   styleUrls: ['./legal-notice.component.scss'],
+  standalone: true,
+  imports: [CardComponent]
 })
 export class LegalNoticeComponent {
 
