e02baab4a39383f0716271a45f6d562ddde2b767
mihaiandreineacsu <mihai@developerakademie.com>
2025-11-23 18:11:20 +0100

    chore: biome format

--- src/app/shared/shared-components/dialogs/add-task-dialog/add-task-dialog.component.ts
+++ src/app/shared/shared-components/dialogs/add-task-dialog/add-task-dialog.component.ts
@@ -1,69 +1,72 @@
-import { type ChangeDetectorRef, Component, EventEmitter, Inject, type OnInit } from '@angular/core';
+import {
+	type ChangeDetectorRef,
+	Component,
+	EventEmitter,
+	Inject,
+	type OnInit,
+} from "@angular/core";
 
-import type { FormControlStatus } from '@angular/forms';
-import { MAT_DIALOG_DATA, type MatDialogRef } from '@angular/material/dialog';
-import { AddTaskComponent } from 'src/app/admin/add-task/add-task.component';
-import type { TaskMode } from 'src/app/admin/add-task/add-task.module';
-import type { TaskRequest, TaskResponse } from 'src/app/shared/models/task.model';
-import { MaterialModule } from 'src/app/shared/modules/material/material.module';
-import { DialogComponent } from '../../dialog/dialog.component';
+import type { FormControlStatus } from "@angular/forms";
+import { MAT_DIALOG_DATA, type MatDialogRef } from "@angular/material/dialog";
+import { AddTaskComponent } from "src/app/admin/add-task/add-task.component";
+import type { TaskMode } from "src/app/admin/add-task/add-task.module";
+import type {
+	TaskRequest,
+	TaskResponse,
+} from "src/app/shared/models/task.model";
+import { MaterialModule } from "src/app/shared/modules/material/material.module";
+import { DialogComponent } from "../../dialog/dialog.component";
 
 export interface AddTaskDialogData {
-  task: TaskResponse,
-  mode: TaskMode
+	task: TaskResponse;
+	mode: TaskMode;
 }
 
 @Component({
-    selector: 'app-add-task-dialog',
-    templateUrl: './add-task-dialog.component.html',
-    styleUrls: ['./add-task-dialog.component.scss'],
-    imports: [
-    DialogComponent,
-    AddTaskComponent,
-    MaterialModule
-]
+	selector: "app-add-task-dialog",
+	templateUrl: "./add-task-dialog.component.html",
+	styleUrls: ["./add-task-dialog.component.scss"],
+	imports: [DialogComponent, AddTaskComponent, MaterialModule],
 })
 export class AddTaskDialogComponent implements OnInit {
-  task!: TaskResponse;
-  mode: TaskMode = 'add';
-  formStatus!: FormControlStatus;
-  clearTaskForm = new EventEmitter();
-  deleteTask = new EventEmitter();
-  submitTaskForm = new EventEmitter();
-  predefinedTaskRequest!: Partial<TaskRequest>;
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: AddTaskDialogData,
-    private dialogRef: MatDialogRef<AddTaskDialogComponent>,
-    private cdr: ChangeDetectorRef
-  ) { }
+	task!: TaskResponse;
+	mode: TaskMode = "add";
+	formStatus!: FormControlStatus;
+	clearTaskForm = new EventEmitter();
+	deleteTask = new EventEmitter();
+	submitTaskForm = new EventEmitter();
+	predefinedTaskRequest!: Partial<TaskRequest>;
+	constructor(
+		@Inject(MAT_DIALOG_DATA) public data: AddTaskDialogData,
+		private dialogRef: MatDialogRef<AddTaskDialogComponent>,
+		private cdr: ChangeDetectorRef,
+	) {}
 
-  ngOnInit(): void {
-    this.task = this.data?.task;
-    this.cdr.detectChanges();
-    this.mode = this.data?.mode || 'add';
-  }
+	ngOnInit(): void {
+		this.task = this.data?.task;
+		this.cdr.detectChanges();
+		this.mode = this.data?.mode || "add";
+	}
 
-  updateFormStatus(eventStatus: FormControlStatus) {
-    this.formStatus = eventStatus;
-  }
+	updateFormStatus(eventStatus: FormControlStatus) {
+		this.formStatus = eventStatus;
+	}
 
-  get title() {
-    if (this.mode === 'add')
-      return 'Add Task';
-    if (this.mode === 'edit')
-      return 'Edit Task';
-    return '';
-  }
+	get title() {
+		if (this.mode === "add") return "Add Task";
+		if (this.mode === "edit") return "Edit Task";
+		return "";
+	}
 
-  handleEditedTask(editedTask: TaskResponse) {
-    this.dialogRef.close(editedTask);
-  }
+	handleEditedTask(editedTask: TaskResponse) {
+		this.dialogRef.close(editedTask);
+	}
 
-  handleAddedTask(addedTask: TaskResponse) {
-    this.dialogRef.close(addedTask);
-  }
+	handleAddedTask(addedTask: TaskResponse) {
+		this.dialogRef.close(addedTask);
+	}
 
-  handleDeletedTask(deletedTaskId: number) {
-    this.dialogRef.close(deletedTaskId);
-  }
+	handleDeletedTask(deletedTaskId: number) {
+		this.dialogRef.close(deletedTaskId);
+	}
 }
--- src/app/shared/shared-components/image-cropper/image-cropper.component.ts
+++ src/app/shared/shared-components/image-cropper/image-cropper.component.ts
@@ -1,127 +1,149 @@
-import { CommonModule } from '@angular/common';
-import { Component, type ElementRef, Optional, ViewChild } from '@angular/core';
-import type { MatDialogRef } from '@angular/material/dialog';
-import type { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { FileUploader, type FileUploaderOptions, FileUploadModule } from 'ng2-file-upload';
-import { ImageCropperComponent } from 'ngx-smart-cropper';
-import type { ScrumApiService } from 'src/app/scrum-api/scrum-api.service';
-import { PROFILE_IMAGE_ENDPOINT } from 'src/app/scrum-api/scrum-profile/scrum-profile.service';
-import { MaterialModule } from '../../modules/material/material.module';
-import type { FeedbackService } from '../../shared-services/feedback/feedback.service';
-import { DialogComponent } from '../dialog/dialog.component';
+import { CommonModule } from "@angular/common";
+import { Component, type ElementRef, Optional, ViewChild } from "@angular/core";
+import type { MatDialogRef } from "@angular/material/dialog";
+import type { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import {
+	FileUploader,
+	type FileUploaderOptions,
+	FileUploadModule,
+} from "ng2-file-upload";
+import { ImageCropperComponent } from "ngx-smart-cropper";
+import type { ScrumApiService } from "src/app/scrum-api/scrum-api.service";
+import { PROFILE_IMAGE_ENDPOINT } from "src/app/scrum-api/scrum-profile/scrum-profile.service";
+import { MaterialModule } from "../../modules/material/material.module";
+import type { FeedbackService } from "../../shared-services/feedback/feedback.service";
+import { DialogComponent } from "../dialog/dialog.component";
 
 const URL = PROFILE_IMAGE_ENDPOINT;
 
 @Component({
-    selector: 'app-image-cropper',
-    imports: [CommonModule, ImageCropperComponent, DialogComponent, MaterialModule, FileUploadModule],
-    templateUrl: './image-cropper.component.html',
-    styleUrls: ['./image-cropper.component.scss']
+	selector: "app-image-cropper",
+	imports: [
+		CommonModule,
+		ImageCropperComponent,
+		DialogComponent,
+		MaterialModule,
+		FileUploadModule,
+	],
+	templateUrl: "./image-cropper.component.html",
+	styleUrls: ["./image-cropper.component.scss"],
 })
 export class ProfileImageCropperComponent {
-  @ViewChild('inputImage') inputImage!: ElementRef<HTMLInputElement>;
-  @ViewChild('imageCropper') imageCropper!: ImageCropperComponent;
+	@ViewChild("inputImage") inputImage!: ElementRef<HTMLInputElement>;
+	@ViewChild("imageCropper") imageCropper!: ImageCropperComponent;
 
-  uploader!: FileUploader;
-  hasBaseDropZoneOver!: boolean;
-  response!: string;
-  fileToChange !: File;
-  fileToUpload !: File | Blob | string | undefined | null;
-  imageChangedEvent !: Event;
-  croppedImage: SafeUrl = '';
+	uploader!: FileUploader;
+	hasBaseDropZoneOver!: boolean;
+	response!: string;
+	fileToChange!: File;
+	fileToUpload!: File | Blob | string | undefined | null;
+	imageChangedEvent!: Event;
+	croppedImage: SafeUrl = "";
 
-  constructor(
-    private sanitizer: DomSanitizer,
-    private scrumApi: ScrumApiService,
-    private feedbackService: FeedbackService,
-    @Optional() private dialogRef?: MatDialogRef<ImageCropperComponent>
-  ) {
-    this.uploader = new FileUploader({
-      url: URL,
-      authToken: `Token ${this.scrumApi.token}`,
-      disableMultipart: false, // Enable multipart
-      itemAlias: 'image', // This should match the name expected on the server side
-    } as FileUploaderOptions);
-    this.hasBaseDropZoneOver = false;
-    this.response = '';
-    this.uploader.response.subscribe(res => this.response = res);
-  }
+	constructor(
+		private sanitizer: DomSanitizer,
+		private scrumApi: ScrumApiService,
+		private feedbackService: FeedbackService,
+		@Optional() private dialogRef?: MatDialogRef<ImageCropperComponent>,
+	) {
+		this.uploader = new FileUploader({
+			url: URL,
+			authToken: `Token ${this.scrumApi.token}`,
+			disableMultipart: false, // Enable multipart
+			itemAlias: "image", // This should match the name expected on the server side
+		} as FileUploaderOptions);
+		this.hasBaseDropZoneOver = false;
+		this.response = "";
+		this.uploader.response.subscribe((res) => (this.response = res));
+	}
 
-  fileOverBase(e: boolean): void {
-    console.log(typeof e)
-    this.hasBaseDropZoneOver = e;
-  }
+	fileOverBase(e: boolean): void {
+		console.log(typeof e);
+		this.hasBaseDropZoneOver = e;
+	}
 
-  fileDropBase(event: File[]) {
-    this.fileToChange = event[0];
-  }
+	fileDropBase(event: File[]) {
+		this.fileToChange = event[0];
+	}
 
-  fileChangeEvent(event: Event): void {
-    this.imageChangedEvent = event;
-  }
+	fileChangeEvent(event: Event): void {
+		this.imageChangedEvent = event;
+	}
 
-  imageSource: string | null = null;
+	imageSource: string | null = null;
 
-  onFileChange(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    if (!input.files?.length) return;
+	onFileChange(event: Event): void {
+		const input = event.target as HTMLInputElement;
+		if (!input.files?.length) return;
 
-    const file = input.files[0];
-    const reader = new FileReader();
-    reader.onload = (e: any) => (this.imageSource = e.target.result);
-    reader.readAsDataURL(file);
-  }
+		const file = input.files[0];
+		const reader = new FileReader();
+		reader.onload = (e: any) => (this.imageSource = e.target.result);
+		reader.readAsDataURL(file);
+	}
 
-  imageCropped(event: string) {
-    // Convert Base64 string to a Blob
-    const base64ToBlob = (base64: string, contentType: string = '', sliceSize: number = 512): Blob => {
-      const byteCharacters = atob(base64.split(',')[1]);
-      const byteArrays = [];
+	imageCropped(event: string) {
+		// Convert Base64 string to a Blob
+		const base64ToBlob = (
+			base64: string,
+			contentType: string = "",
+			sliceSize: number = 512,
+		): Blob => {
+			const byteCharacters = atob(base64.split(",")[1]);
+			const byteArrays = [];
 
-      for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-        const slice = byteCharacters.slice(offset, offset + sliceSize);
-        const byteNumbers = new Array(slice.length);
-        for (let i = 0; i < slice.length; i++) {
-          byteNumbers[i] = slice.charCodeAt(i);
-        }
-        const byteArray = new Uint8Array(byteNumbers);
-        byteArrays.push(byteArray);
-      }
+			for (
+				let offset = 0;
+				offset < byteCharacters.length;
+				offset += sliceSize
+			) {
+				const slice = byteCharacters.slice(offset, offset + sliceSize);
+				const byteNumbers = new Array(slice.length);
+				for (let i = 0; i < slice.length; i++) {
+					byteNumbers[i] = slice.charCodeAt(i);
+				}
+				const byteArray = new Uint8Array(byteNumbers);
+				byteArrays.push(byteArray);
+			}
 
-      return new Blob(byteArrays, { type: contentType });
-    };
-    // Convert the Base64 string to a Blob
-    const blob = base64ToBlob(event, 'image/png');
-    // Convert the blob to a file
-    const blobToFile = (blob: Blob, name: string): File => {
-      const file = new File([blob], name, { type: blob.type });
-      return file;
-    };
+			return new Blob(byteArrays, { type: contentType });
+		};
+		// Convert the Base64 string to a Blob
+		const blob = base64ToBlob(event, "image/png");
+		// Convert the blob to a file
+		const blobToFile = (blob: Blob, name: string): File => {
+			const file = new File([blob], name, { type: blob.type });
+			return file;
+		};
 
-    // Create a file from the cropped image blob
-    const croppedFile = blobToFile(blob, "profile.png"); // You can dynamically generate or allow the user to input a filename
-    // Instead of directly manipulating the queue
-    this.uploader.clearQueue();
-    this.uploader.addToQueue([croppedFile]);
-    // this.uploader.queue = [fileItem];
-    this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(event);
-    // event.blob can be used to upload the cropped image
-  }
+		// Create a file from the cropped image blob
+		const croppedFile = blobToFile(blob, "profile.png"); // You can dynamically generate or allow the user to input a filename
+		// Instead of directly manipulating the queue
+		this.uploader.clearQueue();
+		this.uploader.addToQueue([croppedFile]);
+		// this.uploader.queue = [fileItem];
+		this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(event);
+		// event.blob can be used to upload the cropped image
+	}
 
-  loadImageFailed() {
-    this.feedbackService.openSnackBar(
-      "Failed to load the image. Please ensure the file is a supported format like PNG or JPG.",
-      "Close",
-      {}, false
-    );
-  }
+	loadImageFailed() {
+		this.feedbackService.openSnackBar(
+			"Failed to load the image. Please ensure the file is a supported format like PNG or JPG.",
+			"Close",
+			{},
+			false,
+		);
+	}
 
-  saveImage() {
-    this.uploader.queue[0].upload();
-    this.uploader.queue[0].onSuccess = () => this.dialogRef?.close(true);
-    this.uploader.queue[0].onError = (response) => {
-      const responseJSON = JSON.parse(response);
-      this.feedbackService.openSnackBar(`${responseJSON.detail || 'Something went wrong!'}`, 'Close')
-    };
-  }
+	saveImage() {
+		this.uploader.queue[0].upload();
+		this.uploader.queue[0].onSuccess = () => this.dialogRef?.close(true);
+		this.uploader.queue[0].onError = (response) => {
+			const responseJSON = JSON.parse(response);
+			this.feedbackService.openSnackBar(
+				`${responseJSON.detail || "Something went wrong!"}`,
+				"Close",
+			);
+		};
+	}
 }
