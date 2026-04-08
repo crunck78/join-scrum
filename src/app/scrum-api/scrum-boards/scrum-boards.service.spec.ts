b517cbe4ddffd8550e6812a9d8a526829e4fc3ea
mihaiandreineacsu <mihai@developerakademie.com>
2026-04-08 22:02:45 +0200

    chore: refactor

--- src/app/scrum-api/scrum-boards/scrum-boards.service.spec.ts
+++ src/app/scrum-api/scrum-boards/scrum-boards.service.spec.ts
@@ -9,43 +9,48 @@ import {
 import { TestBed } from "@angular/core/testing";
 import { firstValueFrom } from "rxjs";
 import { Board, BoardResponseAPI } from "../../shared/models/board.model";
+import { CategoryResponseAPI } from "../../shared/models/category.model";
+import { ContactResponseAPI } from "../../shared/models/contact.model";
 import { ListResponseAPI } from "../../shared/models/list.model";
+import { SubtaskResponseAPI } from "../../shared/models/subtask.model";
 import { TaskResponseAPI } from "../../shared/models/task.model";
 import { BOARDS_ENDPOINT, ScrumBoardsService } from "./scrum-boards.service";
 
+const mockAssignee: ContactResponseAPI = {
+	created_at: "",
+	email: "test.user@example.local",
+	id: 1,
+	name: "John Doe",
+	phone_number: "01555555555",
+	updated_at: "",
+};
+
+const mockCategory: CategoryResponseAPI = {
+	color: "#ffffff",
+	created_at: "",
+	id: 1,
+	name: "IT",
+	update_at: "",
+};
+
+const mockSubtask: SubtaskResponseAPI = {
+	created_at: "",
+	done: false,
+	id: 1,
+	title: "Unit tests",
+	updated_at: "",
+};
+
 const mockTask: TaskResponseAPI = {
-	assignees: [
-		{
-			created_at: "",
-			email: "test.user@example.local",
-			id: 1,
-			name: "John Doe",
-			phone_number: "01555555555",
-			updated_at: "",
-		},
-	],
-	category: {
-		color: "#ffffff",
-		created_at: "",
-		id: 1,
-		name: "IT",
-		update_at: "",
-	},
+	assignees: [mockAssignee],
+	category: mockCategory,
 	created_at: "",
 	description: "Some Bug to fix",
 	due_date: "",
 	id: 1,
 	position: 1,
 	priority: "Low",
-	subtasks: [
-		{
-			created_at: "",
-			done: false,
-			id: 1,
-			title: "Unit tests",
-			updated_at: "",
-		},
-	],
+	subtasks: [mockSubtask],
 	title: "Fix the bug",
 	updated_at: "",
 };
